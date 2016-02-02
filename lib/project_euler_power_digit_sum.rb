6a97f9319d9ad0e25b065e82b4dafbf4cd3f9950
bedwardsphoto <brian@brianedwardsphotography.com>
2016-02-02 11:34:40 -0500

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,12 @@
-# code your solution here
+def power_digit_sum(x,n)
+base=x
+power=n
+single_digits=[]
+sum=0
+total=base**power
+single_digits=(total.to_s).split('')
+single_digits.each do |i|
+sum+= i.to_i
+end
+return sum
+end
